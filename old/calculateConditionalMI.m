e6b8181aee093fcc3873ab01896510f1d364ce3e
Axel Faes <axel.faes@gmail.com>
2018-09-19 16:05:27 +0200

    final update

--- old/calculateConditionalMI.m
+++
@@ -1,6 +0,0 @@
-function [entropy] = calculateConditionalMI(input1, input2, input3, nbBins)
-    entro1 = calculateConditionalEntropy(input1, input3, nbBins);
-    entro2 = calculateConditionalEntropy(input2, input3, nbBins);
-    entroJoint = calculateJointConditionalEntropy(input1, input2, input3, nbBins);
-    entropy = entro1 + entro2 - entroJoint;
-end
--- old/calculateMultivariateMI.m
+++
@@ -1,9 +0,0 @@
-function [entropy] = calculateMultivariateMI(nbBins, varargin)
-    amount = nargin - 1;
-    if amount == 2
-        entropy = calculateMutualInformation(varargin{1}, varargin{2}, nbBins);
-    else 
-        last = 1
-        entropy = calculateMultivariate(nbBins, varargin) - calculateMultivariateConditional(nbBins, last, varargin);
-    end
-end
